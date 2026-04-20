142f3f2e20d73a71a49b7041c6eede5dd5b61ff8
Jaber Hassan <jaberkgbd@gmail.com>
2026-04-20 07:34:42 +0600

    Create Line graphics drawing.c

---
+++ Line graphics drawing.c
@@ -0,0 +1,13 @@
+#include <graphics.h>
+#include <conio.h>
+
+int main() {
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, "C:\\TC\\BGI"); 
+    
+    line(100, 100, 300, 300); 
+    
+    getch();
+    closegraph();
+    return 0;
+}
