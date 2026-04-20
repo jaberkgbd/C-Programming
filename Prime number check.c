e2127a6eb24b8061f0a13734485d68f7315f5248
Jaber Hassan <jaberkgbd@gmail.com>
2026-04-20 07:25:25 +0600

    Create Prime number check.c

---
+++ Prime number check.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+    int n=7, flag=0;
+    for(int i=2; i<=n/2; i++) if(n%i==0) flag=1;
+    if(flag==0) printf("Prime"); else printf("Not Prime");
+    return 0;
+}
