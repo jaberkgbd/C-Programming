629d3597df23d4b889b381a5ac9d618422ec6ccb
Jaber Hassan <jaberkgbd@gmail.com>
2026-04-20 07:08:31 +0600

    Create Viewing data size with the sizeof operator.c

---
+++ Viewing data size with the sizeof operator.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+    printf("Size of int: %d bytes\n", sizeof(int));
+    printf("Size of float: %d bytes\n", sizeof(float));
+    printf("Size of char: %d bytes\n", sizeof(char));
+    return 0;
+}
